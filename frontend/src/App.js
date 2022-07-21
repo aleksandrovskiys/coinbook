a6d03b008c728c3b02bd5ffb1e35e6be53a232d7
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-07-21 22:47:35 +0300

    basic navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import NavigationBar from "./components/navigation/NavigationBar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Logout from "./components/Logout";
+import MainPage from "./components/MainPage";
+import Profile from "./components/Profile";
+import Settings from "./components/Settings";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Personal finances</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="App">
+                <NavigationBar/>
+            </div>
+            <Routes>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/logout" element={<Logout/>}/>
+                <Route path="/settings" element={<Settings/>}/>
+                <Route path="/" element={<MainPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/navigation/NavigationBar.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
+import {useNavigate} from "react-router-dom";
+
+const settings = ['Profile', 'Settings', 'Logout'];
+
+const ResponsiveAppBar = () => {
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const navigate = useNavigate();
+
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+    const goHome = () => {
+        navigate("/");
+    }
+
+    return (<AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <CurrencyExchangeIcon sx={{mr: 1, display: 'flex', cursor: "pointer"}} onClick={goHome}/>
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    onClick={goHome}
+                    sx={{
+                        mr: 2,
+                        display: 'flex',
+                        flexGrow: 1,
+                        fontFamily: 'monospace',
+                        fontWeight: 700,
+                        letterSpacing: '.3rem',
+                        color: 'inherit',
+                        textDecoration: 'none',
+                        cursor: "pointer"
+                    }}
+                >
+                    Finance
+                </Typography>
+                <Box sx={{flexGrow: 0}}>
+                    <Tooltip title="Open settings">
+                        <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
+                        </IconButton>
+                    </Tooltip>
+                    <Menu
+                        sx={{mt: '45px'}}
+                        id="menu-appbar"
+                        anchorEl={anchorElUser}
+                        anchorOrigin={{
+                            vertical: 'top', horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top', horizontal: 'right',
+                        }}
+                        open={Boolean(anchorElUser)}
+                        onClose={handleCloseUserMenu}
+                    >
+                        {settings.map((setting) => (
+                            <MenuItem key={setting} data-pointer={setting.toLowerCase()} onClick={
+                                (e) => {
+                                    handleCloseUserMenu();
+                                    navigate(e.target.dataset.pointer);
+                                }
+                            }>
+                                <Typography textAlign="center"
+                                            data-pointer={setting.toLowerCase()}>{setting}</Typography>
+                            </MenuItem>))}
+                    </Menu>
+                </Box>
+            </Toolbar>
+        </Container>
+    </AppBar>);
+};
+export default ResponsiveAppBar;
