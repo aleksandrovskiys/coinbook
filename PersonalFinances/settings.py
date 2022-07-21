0283a5d6966e01fe1dd5d20c448a2a7fa1ad932c
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-07-21 22:50:20 +0300

    django update envs

--- PersonalFinances/settings.py
+++ PersonalFinances/settings.py
@@ -55,7 +55,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_NAME"),
         "USER": os.getenv("POSTGRES_USER"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_URL"),
+        "HOST": os.getenv("POSTGRES_HOST"),
         "PORT": os.getenv("POSTGRES_PORT"),
     }
 }
