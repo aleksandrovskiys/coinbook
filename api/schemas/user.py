e800e48fd8bf6acaeecc5f0a28d7e711d80c848d
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-07-22 10:05:45 +0300

    apply isort

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel, constr
+from pydantic import BaseModel
 from pydantic import EmailStr
-from pydantic import Field
+from pydantic import constr
 
 
 class UserBase(BaseModel):
--- migrations/versions/e6bf915b5c90_.py
+++ migrations/versions/e6bf915b5c90_.py
@@ -5,9 +5,8 @@ Revises:
 Create Date: 2022-07-07 16:48:54.310780
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "e6bf915b5c90"
