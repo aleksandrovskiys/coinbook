9dc7e3bf86069a5203666b1ca4a6e842c0758251
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-08-21 20:10:08 +0300

    remove django app

--- docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /opt/personal_finances;
-python manage.py makemigrations;
-python manage.py migrate;
-python manage.py createcachetable;
-python manage.py runserver 0.0.0.0:8080 --noreload
