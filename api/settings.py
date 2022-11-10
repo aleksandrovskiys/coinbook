e0a10405d019a5c7a9704587641eba1b5ff75fe5
Semyon Aleksandrovskiy <psifisher@gmail.com>
2022-11-11 01:08:42 +0200

    add cors env var transformation

--- api/settings.py
+++ api/settings.py
@@ -1,4 +1,7 @@
+from pydantic import AnyHttpUrl
 from pydantic import BaseSettings
+from pydantic import Field
+from pydantic import validator
 
 
 class Settings(BaseSettings):
@@ -7,7 +10,7 @@ class Settings(BaseSettings):
     POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
-    CORS_ORIGINS: list[str]
+    CORS_ORIGINS: str | list[AnyHttpUrl] = Field(..., env="CORS_ORIGINS")
 
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 12
 
@@ -16,6 +19,12 @@ class Settings(BaseSettings):
     @property
     def sqlalchemy_database_uri(self):
         return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    @validator("CORS_ORIGINS", pre=True)
+    def _assemble_cors_origins(cls, cors_origins):  # noqa
+        if isinstance(cors_origins, str):
+            return [item.strip() for item in cors_origins.split(",")]
+        return cors_origins
 
 
 settings = Settings()
