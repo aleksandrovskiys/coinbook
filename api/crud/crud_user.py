6cb909e700226fddcf91d66eb77186ebe87cbb10
Semyon Aleksandrovskiy <psifisher@gmail.com>
2023-01-22 19:13:54 +0200

    restore create user function

--- api/crud/crud_user.py
+++ api/crud/crud_user.py
@@ -4,12 +4,25 @@ from api.crud.base import CRUDBase
 from api.models.user import User
 from api.schemas.user import UserBase
 from api.schemas.user import UserCreate
+from api.security import get_password_hash
 from api.security import verify_password
 
 
 class UserCrud(CRUDBase[User, UserCreate, UserBase]):
     def __init__(self) -> None:
         self.model = User
+
+    def create(self, session: Session, *, obj_in: UserCreate) -> User:
+        obj_in = self.model(
+            email=obj_in.email,
+            password=get_password_hash(obj_in.password),
+            first_name=obj_in.first_name,
+            last_name=obj_in.last_name,
+        )
+
+        session.add(obj_in)
+        session.commit()
+        return obj_in
 
     def get_by_email(self, session: Session, email: str) -> User | None:
         user = session.query(User).filter(self.model.email == email).first()
